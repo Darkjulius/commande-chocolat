10c918b4192b22af241418e25acc2345a197d07d
Brice THIBAULT <70805751+Darkjulius@users.noreply.github.com>
2023-01-02 18:23:41 +0100

    MEP action.php
    Mise en place du fichier action.php avec son style dans le fichier CSS

---
+++ action.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Action</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <section>
+        <h1>Merci pour votre commande</h1>
+        <ul>
+            <li>Nombre de boîtes commandées: </li>
+            <li>Tarif unitaire: 20 &euro;</li>
+            <li>Total à payer: </li>
+            <li>Remise: euros</li>
+            <li>Nouveau total à payer</li>
+        </ul>
+    </section>
+</body>
+
+</html>
